535a919fcc08ad28b1f22077096a17fe5cd6d6ed
ian612 <34078802+ian612@users.noreply.github.com>
2024-06-04 09:05:14 -0400

    Completed step 2 and 3 of the agb tutorial

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,70 @@
 #![cfg_attr(test, reexport_test_harness_main = "test_main")]
 #![cfg_attr(test, test_runner(agb::test_runner::test_runner))]
 
+use agb::{
+    include_aseprite,
+    display::object::{Graphics, Tag}
+};
+
+// Import the sprites in to this static. This holds the sprite
+// and palette data in a way that is manageable by agb.
+static GRAPHICS: &Graphics = include_aseprite!("gfx/sprites.aseprite");
+
+// We define some easy ways of referencing the sprites
+static PADDLE_END: &Tag = GRAPHICS.tags().get("Paddle End");
+static PADDLE_MID: &Tag = GRAPHICS.tags().get("Paddle Mid");
+static BALL: &Tag = GRAPHICS.tags().get("Ball");
+
 // The main function must take 1 arguments and never return. The agb::entry decorator
 // ensures that everything is in order. `agb` will call this after setting up the stack
 // and interrupt handlers correctly. It will also handle creating the `Gba` struct for you.
 #[agb::entry]
 fn main(mut gba: agb::Gba) -> ! {
-    loop {} // infinite loop for now
+    // Get the object manager
+    let object = gba.display.object.get_managed();
+
+    // Create an object with the ball sprite
+    let mut ball = object.object_sprite(BALL.sprite(0));
+
+    // Input controller
+    use agb::input::Button;
+    let mut input = agb::input::ButtonController::new();
+
+    // Place this at some point on the screen, (50, 50) for example
+    ball.set_x(50).set_y(50).show();
+
+    // Now commit the object controller so this change is reflected on the screen.
+    // This isn't how we will do this in the final version of the code, but will do
+    // for this example.
+    let mut ball_x = 50;
+    let mut ball_y = 50;
+
+    // now we initialise the x and y velocities to 0 rather than 1
+    let mut x_velocity = 0;
+    let mut y_velocity = 0;
+
+    loop {
+        ball_x = (ball_x + x_velocity).clamp(0, agb::display::WIDTH - 16);
+        ball_y = (ball_y + y_velocity).clamp(0, agb::display::HEIGHT - 16);
+
+        // x_tri and y_tri describe with -1, 0 and 1 which way the d-pad
+        // buttons are being pressed
+        x_velocity = input.x_tri() as i32;
+        y_velocity = input.y_tri() as i32;
+
+        if input.is_pressed(Button::A) {
+            // the A button is pressed
+            x_velocity = x_velocity*2;
+            y_velocity = y_velocity*2;
+        }
+
+        ball.set_x(ball_x as u16).set_y(ball_y as u16);
+
+        agb::display::busy_wait_for_vblank();
+        object.commit();
+
+        // We must call input.update() every frame otherwise it won't update based
+        // on the actual button press state.
+        input.update();
+    }
 }
